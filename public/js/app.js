6a15ceb80dd97d6ad3434bd92e8ecd4589d6e8b8
anicdao <amielnicdao@gmail.com>
2020-01-20 19:53:03 -0500

    did basic html page, added js files, and hw requirements

---
+++ public/js/app.js
@@ -0,0 +1,7 @@
+// HW requirements
+// 1. I should be able to scrape from a website. I will be scraping from either hacker news, techcrunch, or geekwire.
+// 2. Be able to store data in database.
+// 3. Users should be able to comment and the comments be saved in database associated with the article.
+// 4. Users should be able to delete comments.
+// 5. Users should be able to see all comments.
+// 6. Minimum: headline, summary, url. Can include pictures. 
