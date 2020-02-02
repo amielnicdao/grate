05bcc4535d3d665d5b01bf337dcb9e62ba253dd9
anicdao <amielnicdao@gmail.com>
2020-02-02 16:14:11 -0500

    working modal, added delete route, comments working now

--- public/js/app.js
+++ public/js/app.js
@@ -1,76 +1,65 @@
-// Grab the articles as a json
-$.getJSON("/articles", function(data) {
-  // For each one
-  for (var i = 0; i < data.length; i++) {
-    // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].headline + "<br />" + data[i].url + "</p>");
-  }
+$(function () {
+	// ON CLICK OF 'GET SCRAPIN'! BUTTON
+	$("#getScrapin").on("click", function () {
+		$.getJSON("/articles", function (data) {
+			for (var i = 0; i < data.length; i++) {
+				$("#articles").append("<h3 data-id='" + data[i]._id + "'>" + data[i].headline + "</h3>" + data[i].summary + "..." + " ");
+				$("#articles").append("<a href = '" + data[i].url + "'</a>" + "<button data-id='" + data._id + "' id='goToArticle'>Go To Article</button>" + " ");
+				$("#articles").append("<data-id='" + data[i]._id + "'>" + "<button type='button' data-id='" + data[i]._id + "' data-toggle='modal' data-target='#commentModal' id='comment-btn'>View Comments</button>");
+			}
+			$("#clear").on("click", function () {
+				$("#articles").empty();
+			})
+		});
+	});
+
+	// VIEW COMMENTS BUTTON
+	$(document).on("click", "#comment-btn", function () {
+		$(".modal-body").empty();
+		let thisId = $(this).attr("data-id");
+		$("#save").attr("data-id", thisId);
+		$.ajax({
+			method: "GET",
+			url: `/articles/${thisId}`
+		}).then(data => {
+			modal(data);
+		});
+	});
+
+	// SAVE COMMENT BUTTON
+	$(document).on("click", "#save", function () {
+
+		let thisId = $(this).attr("data-id");
+		let title = $("#titleInput").val();
+		let body = $("#bodyInput").val();
+
+		$.ajax({
+			method: "POST",
+			url: `/articles/${thisId}`,
+			data: {
+				title: title,
+				body: body
+			}
+		})
+	});
+
+	// DELETE A COMMENT
+	$(document).on("click", "#deleteComment", function () {
+		let thisId = $(this).attr("data-id");
+		$.ajax({
+			method: "DELETE",
+			url: `/notes/${thisId}`
+		})
+	});
 });
 
-
-// Whenever someone clicks a p tag
-$(document).on("click", "p", function() {
-  // Empty the notes from the note section
-  $("#notes").empty();
-  // Save the id from the p tag
-  var thisId = $(this).attr("data-id");
-
-  // Now make an ajax call for the Article
-  $.ajax({
-    method: "GET",
-    url: "/articles/" + thisId
-  })
-    // With that done, add the note information to the page
-    .then(function(data) {
-      console.log(data);
-      // The title of the article
-      $("#notes").append("<h2>" + data.title + "</h2>");
-      // An input to enter a new title
-      $("#notes").append("<input id='titleinput' name='title' >");
-      // A textarea to add a new note body
-      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-      // A button to submit a new note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-
-      // If there's a note in the article
-      if (data.note) {
-        // Place the title of the note in the title input
-        $("#titleinput").val(data.note.title);
-        // Place the body of the note in the body textarea
-        $("#bodyinput").val(data.note.body);
-      }
-    });
-});
-
-// When you click the savenote button
-$(document).on("click", "#savenote", function() {
-  // Grab the id associated with the article from the submit button
-  var thisId = $(this).attr("data-id");
-
-  // Run a POST request to change the note, using what's entered in the inputs
-  $.ajax({
-    method: "POST",
-    url: "/articles/" + thisId,
-    data: {
-      // Value taken from title input
-      title: $("#titleinput").val(),
-      // Value taken from note textarea
-      body: $("#bodyinput").val()
-    }
-  })
-    // With that done
-    .then(function(data) {
-      // Log the response
-      console.log(data);
-      // Empty the notes section
-      $("#notes").empty();
-    });
-
-  // Also, remove the values entered in the input and textarea for note entry
-  $("#titleinput").val("");
-  $("#bodyinput").val("");
-});
-
-
-
+function modal(data) {
+	$("#modalLabel").text(data.headline);
+	$(".modal-body").append(`<form><div class='form-group'><input type='text' class='form-control' id='titleInput' placeholder='Subject'></div><div class='form-group'><textarea class='form-control' id='bodyInput' placeholder='What are your thoughts?'></textarea></div></form>`);
+	if (data.note.length > 0) {
+		for (let i = 0; i < data.note.length; i++) {
+			$(".modal-body").prepend(`<div class='card'><div class='card-body'><h5 class='card-title'>${data.note[i].title}</h5><p class='card-text'>${data.note[i].body}</p><button class='btn btn-danger' data-dismiss='modal' id='deleteComment' data-id='${data.note[i]._id}'>Delete</button></div></div><br />`);
+		}
+	}
+};
 
